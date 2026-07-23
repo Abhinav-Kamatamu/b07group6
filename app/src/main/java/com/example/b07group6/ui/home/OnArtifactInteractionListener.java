61d82c1a74b0d9bcace6a3011b79f4a94efd0802
Stranger <abhinav.kamatamu@gmail.com>
2026-07-23 05:11:37 -0400

    feat: Recycle Viewer Added!

---
+++ app/src/main/java/com/example/b07group6/ui/home/OnArtifactInteractionListener.java
@@ -0,0 +1,7 @@
+package com.example.b07group6.ui.home;
+
+public interface OnArtifactInteractionListener{
+    void onSingleClick(int position);
+    void onSaveArifactPress(int position);
+    void onItemLongPress(int position);
+}
